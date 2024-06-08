a3cd923229a82ed5cbe23fe7093ee00aee20116a
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-06-08 09:39:05 -0400

    first commit good function practice

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+const array = [1, 2, 3, 4, 5]
+const returnFirstTwoDrivers = (array) =>{
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = (array) =>{
+    return array.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const fare = 10
+const createFareMultiplier = (x) =>{
+    return (fare) =>{
+        return fare *x
+    }
+}
+
+const fareDoubler = (createFareMultiplier) =>{
+    return createFareMultiplier *2
+}
+
+console.log(fareDoubler(fare))
+
+const fareTripler = (createFareMultiplier) =>{
+    return createFareMultiplier * 3
+}
+
+const selectDifferentDrivers = (array, fn) =>{
+    return fn(array)
+}
+
